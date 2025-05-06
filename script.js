697c3c3cfaa66c9ca01cba5f0e6bb206463b83aa
karlzzcoder <kenrayasetiawan177@gmail.com>
2025-05-06 08:23:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const ctaButton = document.getElementById('cta-button');
+    
+    // Event listener untuk tombol "Discover More"
+    ctaButton.addEventListener('click', function () {
+        alert('Thank you for discovering Coca-Cola!');
+    });
+});
